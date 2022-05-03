b724ed05392f79669a4949836e4859e216df22a1
AntonioHincapie <marcohincapiem@gmail.com>
2022-05-03 16:40:03 -0500

    Setting up modules

---
+++ modules/bookslist.js
@@ -0,0 +1,25 @@
+import { dataBooks, books} from "./variables.js";
+import Libro from "./libro.js";
+
+const printList = () => {
+  const libro = new Libro();
+  books.innerHTML = null;
+  if (localStorage.getItem('book')) {
+  }
+  for (let i = 0; i < dataBooks.length; i += 1) {
+    const bookStorage = document.createElement('div');
+    const bookInfo = document.createElement('p');
+    const deleteBtn = document.createElement('button');
+    bookInfo.textContent = `"${dataBooks[i].title}" by ${dataBooks[i].author}`;
+    deleteBtn.textContent = 'Remove';
+    books.appendChild(bookStorage);
+    bookStorage.append(bookInfo, deleteBtn);
+    bookStorage.className = 'bookStorage';
+    deleteBtn.addEventListener('click', () => {
+      bookStorage.remove();
+      libro.deleteBook(i);
+    });
+  }
+};
+
+export default printList;
